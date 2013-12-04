129c54b646fd7e6d4d9acccf0cb6290e059fce14
David Wang <dswang@users.sourceforge.net>
2013-12-05 07:56:33 +1100

    V: switch to common fccomp and lccomp.

--- src/V/writefli.c
+++ src/V/writefli.c
@@ -3,14 +3,12 @@
 
 #include <fcntl.h>
 #include "jimk.h"
+#include "fccomp.h"
 #include "fli.h"
+#include "lccomp.h"
 #include "peekpok_.h"
 
 extern unsigned write(), read();
-
-extern char *dcomp(), *run_comp(), *skip_run_comp(), *bcomp(), *bsrcomp(),
-	*sbsccomp(), *sbsrsccomp(); 
-extern unsigned *fccomp(), *lccomp(), *brun();
 
 static int wfli_fd;
 static struct fli_head head;
