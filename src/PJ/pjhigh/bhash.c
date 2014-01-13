1995f5f827a744d7cd5f0b983c1c476436096a02
David Wang <dswang@users.sourceforge.net>
2014-01-14 08:21:46 +1100

    PJ: convert bclosest_col, with the two-slot caching system.
    Fixed a minor error in the dither error calculation: an arthimetic
    right shift by 2 was used where division by 4 was intended.  The right
    shift causes negative values to round towards negative infinity.

--- src/PJ/pjhigh/bhash.c
+++ src/PJ/pjhigh/bhash.c
@@ -1,38 +1,152 @@
-/*****
- Stuff for 'bhash', which is a system for speeding up finding the
- closest color to a given true color.  It builds up a hash table, so that
- colors previously looked up are likely to be in the hash and need not
- be recomputed.  Typical hit rate is 85% or better, but varies a lot from
- picture to picture.
-
- See also BHASHLKP.ASM which contains the dithering and lookup code.
-
- *******/
+/* bhash.c
+ *
+ * Stuff for 'bhash', which is a system for speeding up finding the
+ * closest color to a given true color.  It builds up a hash table, so that
+ * colors previously looked up are likely to be in the hash and need not
+ * be recomputed.  Typical hit rate is 85% or better, but varies a lot from
+ * picture to picture.
+ *
+ *
+ * int bclosest_col(Rgb3 *rgb, int count, SHORT dither)
+ *
+ *   this routine applies optional error diffusion dithering to the input
+ *   rgb value, then looks up the color using a cached lookup.	if the color
+ *   is not found in the cache, the closestc() routine is called, and the
+ *   color it finds is stored into the cache.  if dithering is requested,
+ *   the weighted difference between the requested and found colors is saved
+ *   for use on the next call.	the cache tends to get hits in the 65-100%
+ *   range, generally on the high end of that range.  (95% is typical except
+ *   when dithering is used.  dithering can generate tens of thousands of
+ *   unique colors, which sometimes flushes the cache too quickly.)
+ *
+ *   the dithering error from the last call is rolled into the rgb values of
+ *   the current call using the following (conceptual) formula:
+ *	  temp = rgb->r + rederr;
+ *	  if (temp < 0)
+ *	    temp = 0;
+ *	  if (temp > 255)
+ *	    temp = 255;
+ *	  drgb.r = temp;
+ *   the same logic is applied to each of the r,g,b components, of course.
+ *   after the error-corrected values are obtained, the rgb pointer parameter
+ *   is modified to point at 'drgb' so that the lookup and post-lookup-
+ *   dithering will use the error-corrected values.
+ *
+ *   the post-lookup-dithering takes the weighted difference between the
+ *   closest color we found and the color we were trying to find, as follows:
+ *	 rederr = 3*(drgb.r - foundrgb->r)/4;
+ *   this formula is applied to each of the components, and the values are
+ *   saved for use in the next call.
+ *
+ *   the cached lookup reduces the 24-bit rgb value to an 18-bit value used
+ *   for the actual lookup (ie, we punt some significance for speed), and a
+ *   12-bit hash key to use in the cache data lookup.  the hash key is made
+ *   up of the four lowest bits of the 6-bit reduced rgb components.  (ie,
+ *   from the original 8-bit components, the hash keys are the bits xxKKKKxx).
+ *   the twelve bit hash key then indexes into the table of 4096 cache
+ *   domains.  each domain contains two slots, and each slot occupies four
+ *   bytes, giving a cache data area of 32k.  within each cache slot, the
+ *   four bytes are mapped out such that the first byte is the index of the
+ *   closest color found to match the rgb value, and the next 3 bytes hold
+ *   the rgb value itself (scaled down to 64-level/6-bit values, remember).
+ *   thus, when loaded as a dword, the closest color index ends up in the low
+ *   byte of the register and the rgb ends up high-aligned in the upper 18
+ *   bits of the register in bgr order.  this doesn't really matter since the
+ *   values in the cache data area are visible only to us in this routine,
+ *   and we always deal with them packed into this particular sequence. (see
+ *   the comments on the barrel-shifter-torture-test code, below).
+ *
+ *   once the 12 bit key is built, it is multiplied by 8 to index to the
+ *   correct domain within the cache data area, and the value from the first
+ *   slot in the domain is loaded.  the rgb value in the slot is compared
+ *   to the 18-bit rgb we're looking for.  if they match, the closest index
+ *   number pulled from that cache slot is returned.  if the rgb value in the
+ *   first cache slot is not a match, we check the other slot.	if that rgb
+ *   value matches, we return that slot's closest index.  if the second slot's
+ *   rgb value doesn't match either, we have a total cache miss.  in this
+ *   case, the data from the first cache slot is transferred to the second
+ *   slot, and the closestc() routine is used to find the color in the color
+ *   table.  the index returned by closestc() is stored into the first cache
+ *   slot.  this shuffling of entries on a cache miss implements a very simple
+ *   (and cheap in terms of cycles) LRU flushing mechanism.
+ *
+ *   there is no special 'valid' field within the cache to mark slots that
+ *   are used; the cache data is sort of self-validating, for lack of a
+ *   better term.  the cache starts out initialized to zeros.  when we
+ *   compare a requested color to the value in a slot that has never been
+ *   used, the values would match only if the color being searched for is
+ *   also all zeros.  this, of course, can only happen for domain zero; in any
+ *   other slot that has been used, the rgb values cannot be all zeros or they
+ *   wouldn't have hashed to that slot.  domain zero is therefore handled as a
+ *   special case.  (if it weren't, a lookup for rgb {0,0,0} could end up
+ *   returning index 0 from the cache, and there's no guarantee that the
+ *   first palette slot holds {0,0,0} really).	the special case handling is
+ *   simple:  the rgb values occupy the upper 18 bits of the dword, and the
+ *   index occupies the lower 8 bits, leaving 6 bits inbetween that will
+ *   always be zero because of the way we pack 24 bits down to 18.  those bits
+ *   are included in the compare of the requested value against the value in
+ *   the cache data, but since it is the requested values that eventually
+ *   end up getting stored as we makes entries in the cache, those bits of
+ *   each dword always remain zeros.  so, when we init the cache (in bhash.c)
+ *   we just set the first two cache slots to values that have one of those
+ *   bits on, which means that the first time a request is made to find color
+ *   {0,0,0}, the compare against what's in the cache slots will be unequal,
+ *   forcing a lookup.	once the lookup has filled in a real value, {0,0,0}
+ *   lookups will work fine after that.
+ *
+ *   well, now the comments are longer than the code, so on with it...
+ *
+ *   oh yeah -- the order of things in the code below is tuned towards the
+ *   idea of fastest performance when dithering is turned off and with the
+ *   assumption that the first cache slot will hold a hit most of the time.
+ *   it looks a bit disordered, but the idea is to take the fall-thru case
+ *   of most branches for the conditions we expect to happen the most.
+ */
 
 #include "errcodes.h"
 #include "pjbasics.h"
 #include "bhash.h"
 
-#define BSIZ (16*16*16*2*sizeof(ULONG)) // ie, 32kbytes,
-										//	 4096 domains of 2 slots of 4 bytes
+/* See comments above. */
+#define SPECIAL_CASE_INIT_VALUE 0x00000100
 
-#define SPECIAL_CASE_INIT_VALUE 0x00000100 // see comments in bhashlkp.asm
+typedef union bhash_slot {
+	struct {
+		unsigned index : 8;
+		int r : 6;
+		int g : 6;
+		int b : 6;
+		unsigned x : 6;
+	} s;
+	uint32_t all;
+} BhashSlot;
+STATIC_ASSERT(bhash, sizeof(BhashSlot) == 4);
 
-//static struct bhash *bhash;
+typedef struct bhash_domain {
+	BhashSlot slot[2];
+} BhashDomain;
 
 typedef struct bhash_control {
-	void   *cachedata;
+	BhashDomain *cachedata;
 	Rgb3   *ctab;
 	int    rederr;
 	int    grnerr;
 	int    bluerr;
-	ULONG  drgb;	  // temp var used by dithering, blackbox to us here
 #ifdef SHOW_STATS
 	int calls, hits1, hits2, fhits, misses;
 #endif
-	} BhashCtl;
+} BhashCtl;
 
-BhashCtl bhashctl;			// global so assembler code can see it.
+/* 32kb hash table: 4096 domains of 2 slots of 4 bytes */
+#define BSIZ (16*16*16*sizeof(BhashDomain))
+
+static BhashCtl bhashctl;
+
+static int
+clamp(int a, int b, int c)
+{
+	return Max(a, Min(b, c));
+}
 
 void free_bhash(void)
 /*****************************************************************************
@@ -59,8 +173,6 @@ Errcode make_bhash(void)
  *
  ****************************************************************************/
 {
-	ULONG *pwrk;
-
 	free_bhash();							// make sure it's gone.
 	clear_mem(&bhashctl, sizeof(bhashctl)); // clean out dithering, etc.
 
@@ -68,9 +180,10 @@ Errcode make_bhash(void)
 		return(Err_no_memory);
 
 	clear_mem(bhashctl.cachedata, BSIZ);	// init cache area to zeros.
-	pwrk = bhashctl.cachedata;				// init the first two slots in
-	pwrk[0] = SPECIAL_CASE_INIT_VALUE;		// the cache to a special value.
-	pwrk[1] = SPECIAL_CASE_INIT_VALUE;		// see comments in bhashlkp.asm.
+
+	/* Init the first two slots in the cache to a special value. */
+	bhashctl.cachedata[0].slot[0].all = SPECIAL_CASE_INIT_VALUE;
+	bhashctl.cachedata[0].slot[1].all = SPECIAL_CASE_INIT_VALUE;
 
 	bhashctl.ctab = vb.pencel->cmap->ctab;	// do all the dereferencing once.
 
@@ -85,8 +198,6 @@ Boolean is_bhash(void)	/* used by GEL tool to see if bhash already in place */
 	return (bhashctl.cachedata != NULL);
 }
 
-#ifdef CCODE	/* this routine moved to bhashlkp.c */
-
 int bclosest_col(register Rgb3 *rgb,int count,SHORT dither)
 /*****************************************************************************
  * find closest color in color map to a true color value,
@@ -97,71 +208,65 @@ int bclosest_col(register Rgb3 *rgb,int count,SHORT dither)
  * flag in the device driver to tell us how to do this.  (<-Yeah!)
  ****************************************************************************/
 {
-register struct bhash *h;
+BhashDomain *h;
 int i;
 int r,g,b;
 int closest;
 Rgb3 drgb;	/* rgb after dither adjustments */
-Rgb3 srgb;	/* scaled down to 0-63 */
+BhashSlot srgb;
 
-	bhashctl.prgb = rgb;
-	bhashctl.ctabcount = count;
-
-	if (dither) {
-		closest = bhash_dithered_lookup();
-	} else {
-		closest = bhash_lookup();
-	}
-	return closest;
-
+	/* Add the dithering error from the last call into the current call. */
 	if (dither)
 	{
-	register int temp;
+		r = clamp(0, rgb->r + bhashctl.rederr, RGB_MAX - 1);
+		g = clamp(0, rgb->g + bhashctl.grnerr, RGB_MAX - 1);
+		b = clamp(0, rgb->b + bhashctl.bluerr, RGB_MAX - 1);
 
-		temp = rgb->r + rerr;
-		if (temp < 0)
-			temp = 0;
-		if (temp > RGB_MAX-1)
-			temp = RGB_MAX-1;
-		drgb.r = r = temp;
-		temp = rgb->g + gerr;
-		if (temp < 0)
-			temp = 0;
-		if (temp > RGB_MAX-1)
-			temp = RGB_MAX-1;
-		drgb.g = g = temp;
-		temp = rgb->b + berr;
-		if (temp < 0)
-			temp = 0;
-		if (temp > RGB_MAX-1)
-			temp = RGB_MAX-1;
-		drgb.b = b = temp;
+		drgb.r = r;
+		drgb.g = g;
+		drgb.b = b;
+
 		rgb = &drgb;
 	}
 
-	srgb.r = rgb->r>>2;
-	srgb.g = rgb->g>>2;
-	srgb.b = rgb->b>>2;
+	/* Look for a cache hit. */
+	srgb.all = 0;
+	srgb.s.r = rgb->r>>2;
+	srgb.s.g = rgb->g>>2;
+	srgb.s.b = rgb->b>>2;
 
-	/* first look for a hash hit */
-	i = ((((srgb.r&0xf)<<8) + ((srgb.g&0xf)<<4) + ((srgb.b&0xf))));
-	h = bhash+i;
-	closest = h->closest;
-	if (closest != 0) {
-		if (h->rgb.r == srgb.r &&  h->rgb.g == srgb.g && h->rgb.b == srgb.b) {
-			goto GOTIT;
-		}
-	}
-	closest = h->closest = closestc(rgb,vb.pencel->cmap->ctab,count);
-	h->rgb = srgb;
+	i = ((((srgb.s.r&0xf)<<8) + ((srgb.s.g&0xf)<<4) + ((srgb.s.b&0xf))));
+	h = bhashctl.cachedata + i;
+
+	/* Check first slot. */
+	srgb.s.index = h->slot[0].s.index;
+	if (srgb.all == h->slot[0].all)
+		goto GOTIT;
+
+	/* Check second slot. */
+	srgb.s.index = h->slot[1].s.index;
+	if (srgb.all == h->slot[1].all)
+		goto GOTIT;
+
+	/* Total cache miss. */
+	srgb.s.index = closestc(rgb, bhashctl.ctab, count);
+	h->slot[1].all = h->slot[0].all;
+	h->slot[0].all = srgb.all;
+
 GOTIT:
+
+	/* Save the dithering error for use in the next call. */
+	closest = srgb.s.index;
 	if (dither)
 	{
-		rgb = vb.pencel->cmap->ctab + closest;
-		rerr = 3*(r - rgb->r)/4;
-		gerr = 3*(g - rgb->g)/4;
-		berr = 3*(b - rgb->b)/4;
+		rgb = bhashctl.ctab + closest;
+
+		/* Note: the original asm used an arithmetic shift here which
+		 * produces different results when the error is negative.
+		 */
+		bhashctl.rederr = 3*(r - rgb->r)/4;
+		bhashctl.grnerr = 3*(g - rgb->g)/4;
+		bhashctl.bluerr = 3*(b - rgb->b)/4;
 	}
 	return(closest);
 }
-#endif /* CCODE */
