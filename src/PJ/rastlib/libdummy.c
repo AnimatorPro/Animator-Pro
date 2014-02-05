75904b2cc075a0d12f5a1e7bf02740e887e93f5d
David Wang <dswang@users.sourceforge.net>
2014-02-06 08:06:14 +1100

    RASTLIB: sluff pj_sdo_nutin.

--- src/PJ/rastlib/libdummy.c
+++ src/PJ/rastlib/libdummy.c
@@ -3,7 +3,11 @@
 #include "libdummy.h"
 
 void pj_vdo_nutin(void) { return; }
+
+#ifdef SLUFFED
 SHORT pj_sdo_nutin(void) { return(0); }
+#endif /* SLUFFED */
+
 Pixel pj_rcdo_nutin(void) { return(0); }
 Errcode pj_errdo_unimpl(void) { return(Err_unimpl); }
 Errcode pj_errdo_success(void) { return(Success); }
