61df11cf2e90fd074505a5f16effd767199de336
kiki <charles@skeletalstudios.com>
2024-04-17 23:33:00 -0400

    Fixing disabling WITH_POCO.

--- src/pj/poco/bcutil.c
+++ src/pj/poco/bcutil.c
@@ -5,45 +5,11 @@
   #define iscsym(c) ( (c) == '_' || isalnum((c)) )
 #endif
 
-#ifdef SLUFFED
-char *po_skip_space(char *line)
-/*****************************************************************************
- * return pointer to next non-white, or NULL if no more non-white on line.
- ****************************************************************************/
-{
-	if (line == NULL)	/* can this happen?  asm codes checks for it. */
-		return NULL;
 
-	while (isspace(*line))
-		++line;
-
-	return (*line) ? line : NULL;
-
-}
-#endif /* SLUFFED */
-
-#ifdef SLUFFED
-int po_hashfunc(unsigned char *s)
-/*****************************************************************************
- * typical hashing function.
- ****************************************************************************/
-{
-
-int acc;
-int c;
-
-acc = *s++;
-while ((c = *s++) != 0)
-	acc = (acc /* <<1 */ )+c;
-return(acc&(HASH_SIZE-1));
-
-}
-#endif /* SLUFFED */
-
-char *po_chop_csym(char *line, char *word, int maxlen, char **wordnext)
 /*****************************************************************************
  * chop the next C symbol into the word buffer, up to maxlen chars.
  ****************************************************************************/
+char *po_chop_csym(char *line, char *word, int maxlen, char **wordnext)
 {
 	while (--maxlen && iscsym(*line)) {
 		*word++ = *line++;
